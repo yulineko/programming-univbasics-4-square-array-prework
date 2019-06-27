0eb724b80b49e2abf40e8e23f9280258abe26cfe
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-06-27 01:14:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,11 +3,10 @@ def square_array(array)
   counter = 0
   new_arr = []
   
-  while array[counter] do
-    new_arr << counter ** 2
+  while count < array.size do
+    new_arr << (array[counter] ** 2)
     counter += 1
-    
-
+  end
+  new_arr
 end
-
 
